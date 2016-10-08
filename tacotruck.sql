c4c3ede0d20b54989acf2f78a7075800015ecf40
smgoy <smgoy31@gmail.com>
2016-10-08 13:42:09 -0700

    completes database setup

--- tacotruck.sql
+++ tacotruck.sql
@@ -3,15 +3,20 @@ CREATE DATABASE tacotrucks;
 
 \c tacotrucks;
 
-CREATE TABLE tacos (
+CREATE TABLE trucks (
   ID SERIAL PRIMARY KEY,
-  name VARCHAR NOT NULL,
-  truck_id INTEGER references trucks(ID) NOT NULL UNIQUE
+  name VARCHAR NOT NULL
 );
 
 CREATE TABLE ingredients (
   ID SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL
+);
+
+CREATE TABLE tacos (
+  ID SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  truck_id INTEGER references trucks(ID) NOT NULL UNIQUE
 );
 
 CREATE TABLE taco_ingredients (
@@ -20,7 +25,15 @@ CREATE TABLE taco_ingredients (
   ingredient_id INTEGER references ingredients(ID) NOT NULL
 );
 
-CREATE TABLE trucks (
-  ID SERIAL PRIMARY KEY,
-  name VARCHAR NOT NULL
-);
+INSERT INTO trucks (name)
+  VALUES ('Taco Bamba');
+
+INSERT INTO ingredients (name)
+  VALUES ('Grilled Chicken'), ('Ancho Mayo'), ('Cucumber Mayo'),
+         ('Chipotle Yogart');
+
+INSERT INTO tacos (name, truck_id)
+  VALUES ('Arabe', 1);
+
+INSERT INTO taco_ingredients (taco_id, ingredient_id)
+  VALUES (1, 1), (1, 2), (1, 3), (1, 4)
