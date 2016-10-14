d97329be32bd9d914f422dc3cafcccf03ad442a9
smgoy <smgoy31@gmail.com>
2016-10-13 22:43:53 -0700

    fixes seed bug

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/tacoTruckAPI');
 
-console.log(mongoose.Truck);
-
 var Truck = require('../app/models/trucks');
 var Taco = require('../app/models/tacos');
 
--- config/seeds.js
+++ config/seeds.js
@@ -1,51 +1,37 @@
-// var Truck = require('../app/models/trucks');
-// var Taco = require('../app/models/tacos');
-//
-// var mongoose = require('mongoose');
-//
-// console.log('connecting to db');
-//
-// mongoose.connect('mongodb://localhost/tacoTruckAPI',function(){
-//
-//   console.log('dropping db...');
-//
-//   mongoose.connection.db.dropDatabase();
+var Truck = require('../app/models/trucks');
+var Taco = require('../app/models/tacos');
 
-  // console.log('seeding db...');
-  // Truck.create({name: 'Taco Bamba'});
-  // Truck.create({name: 'District Taco'});
-  // Taco.create({
-  //   name: 'Arabe',
-  //   truck: 'Taco Bamba',
-  //   ingredients: [
-  //     'Grilled Chicken',
-  //     'Ancho Mayo',
-  //     'Cucumber Mayo',
-  //     'Chipotle Yogart'
-  //   ]
-  // });
-  // Taco.create({
-  //   name: 'Spicy Shroom',
-  //   truck: 'Taco Bamba',
-  //   ingredients: [
-  //     'Chipotle Portabella',
-  //     'Grilled Corn',
-  //     'Cotija',
-  //     'Pepitas'
-  //   ]
-  // });
-  // Taco.create({
-  //   name: 'Al Pastor',
-  //   truck: 'District Taco',
-  //   ingredients: [
-  //     'Pork',
-  //     'Shredded Cheese',
-  //     'Pico de Gallo',
-  //     'Jalepenos'
-  //   ]
-  // });
-  //
-  // console.log('Done!');
-// });
+Truck.create({name: 'Taco Bamba'});
+Truck.create({name: 'District Taco'});
+Taco.create({
+  name: 'Arabe',
+  truck: 'Taco Bamba',
+  ingredients: [
+    'Grilled Chicken',
+    'Ancho Mayo',
+    'Cucumber Mayo',
+    'Chipotle Yogart'
+  ]
+});
+Taco.create({
+  name: 'Spicy Shroom',
+  truck: 'Taco Bamba',
+  ingredients: [
+    'Chipotle Portabella',
+    'Grilled Corn',
+    'Cotija',
+    'Pepitas'
+  ]
+});
+Taco.create({
+  name: 'Al Pastor',
+  truck: 'District Taco',
+  ingredients: [
+    'Pork',
+    'Shredded Cheese',
+    'Pico de Gallo',
+    'Jalepenos'
+  ]
+});
 
-// mongoose.connection.close();
+console.log('seeded!');
