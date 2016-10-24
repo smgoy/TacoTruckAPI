260189a95476031a6b4b705df520a87ff3063cb1
smgoy <smgoy31@gmail.com>
2016-10-24 12:56:55 -0700

    sets up remote db

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tacoTruckAPI');
+mongoose.connect('mongodb://smgoy:password123@ds031167.mlab.com:31167/tacotruckapi');
 
 var Truck = require('../app/models/trucks');
 var Taco = require('../app/models/tacos');
